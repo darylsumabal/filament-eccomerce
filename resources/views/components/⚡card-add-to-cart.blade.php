5aa14e0a84b7b152926d9af7f72bf1e94462ca67
Legendaryl <darylsumabal123@gmail.com>
2026-07-15 11:24:50 +0800

    ok

--- resources/views/components/⚡card-add-to-cart.blade.php
+++ resources/views/components/⚡card-add-to-cart.blade.php
@@ -60,24 +60,30 @@ new class extends Component {
 <div x-data="cart()" class="modal  text-black!" id="coffee-modal" popover>
     <div class="modal-box w-96 bg-[#E2D9C8]! border-2 border-[#e2bf7d]!">
         @if ($selectedCoffee)
-        <h3 class="font-bold text-lg">Add {{ $selectedCoffee->name }} to cart?</h3>
-        <img src="{{ $selectedCoffee->image ? Storage::url($selectedCoffee->image) : asset('/coffee_alt.jpg') }}"
-            alt="coffee.jpg" class="h-90! w-full rounded-md border-2 border-[#e2bf7d]!" />
-        <p class="py-2 text-sm line-clamp-2">{{ $selectedCoffee->description }}</p>
-        <p class="font-black mt-2 text-black!">Price: ₱ {{ $selectedCoffee->price }}</p>
-        <label class="text-black!">Order notes</label>
-        <flux:textarea class="text-black!" rows="auto" wire:model="note" placeholder="Enter note..." />
+            <h3 class="font-bold text-lg">Add {{ $selectedCoffee->name }} to cart?</h3>
+            <img src="{{ $selectedCoffee->image ? Storage::url($selectedCoffee->image) : asset('/coffee_alt.jpg') }}"
+                alt="coffee.jpg" class="h-90! w-full rounded-md border-2 border-[#e2bf7d]!" />
+            <p class="py-2 text-sm line-clamp-2">{{ $selectedCoffee->description }}</p>
+            <p class="font-black mt-2 text-black!">Price: ₱ {{ $selectedCoffee->price }}</p>
+            <label class="text-black!">Order notes:</label>
+            <fieldset class="fieldset">
+                <textarea class="textarea h-24 border-[#e2bf7d]" wire:model="note" placeholder="Enter note..."></textarea>
+            </fieldset>
         @else
-        <p class="py-4">Loading coffee details...</p>
+            <p class="py-4">Loading coffee details...</p>
         @endif
-        <div class="mt-4">
-            <flux:checkbox.group wire:model="addons" label="Addons" class="h-20 overflow-y-scroll ">
+        <div class="mt-4 ">
+            <div class="h-24 overflow-y-scroll ">
                 @foreach ($this->availableAddons as $addon)
-                <flux:checkbox class="text-black!" wire:key="addon-{{ $addon->id }}" label="{{ $addon->name }}"
-                    value="{{ $addon->id }}" />
+                    <label class="flex items-center gap-2 cursor-pointer">
+                        <input wire:model="addons" type="checkbox" wire:key="addon-{{ $addon->id }}"
+                            label="{{ $addon->name }}" value="{{ $addon->id }}"
+                            class="checkbox checkbox-sm border-[#e2bf7d]" />
+                        <span>{{ $addon->name }}</span>
+                        <span class="text-sm ml-auto">₱{{ $addon->price }}</span>
+                    </label>
                 @endforeach
-            </flux:checkbox.group>
-
+            </div>
         </div>
 
         <div class="flex justify-between items-center  mt-4">
@@ -99,27 +105,26 @@ new class extends Component {
 
 
 @verbatim
-<script>
-    document.addEventListener('alpine:init', () => {
-        Alpine.data('cart', () => ({
-            cart: Alpine.$persist([]).as('cart-items'),
+    <script>
+        document.addEventListener('alpine:init', () => {
+            Alpine.data('cart', () => ({
+                cart: Alpine.$persist([]).as('cart-items'),
+                init() {
+                    // Listen to the window event safely
+                    window.addEventListener('add-to-cart', (event) => {
+                        const newItem = event.detail?.item;
 
-            init() {
-                // Listen to the window event safely
-                window.addEventListener('add-to-cart', (event) => {
-                    const newItem = event.detail?.item;
+                        if (newItem) {
+                            this.cart.push(newItem);
 
-                    if (newItem) {
-                        this.cart.push(newItem);
-
-                        // Let the DOM update, then notify the navbar
-                        this.$nextTick(() => {
-                            window.dispatchEvent(new CustomEvent('cart-updated'));
-                        });
-                    }
-                });
-            }
-        }))
-    })
-</script>
+                            // Let the DOM update, then notify the navbar
+                            this.$nextTick(() => {
+                                window.dispatchEvent(new CustomEvent('cart-updated'));
+                            });
+                        }
+                    });
+                }
+            }))
+        })
+    </script>
 @endverbatim
